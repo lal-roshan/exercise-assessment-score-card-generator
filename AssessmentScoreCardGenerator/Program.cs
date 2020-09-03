ec26f44b20a15298ebdf5eb86b0ef28f68c729e7
Roshan Lal <153064@ust-global.com>
2020-09-03 10:26:32 +0000

    Initial commit

--- AssessmentScoreCardGenerator/Program.cs
+++ AssessmentScoreCardGenerator/Program.cs
@@ -13,27 +13,47 @@ namespace AssessmentScoreCardGenerator
 
      * the maximum score can be set as value for enum constants
      */
+    public enum AssessmentType
+    {
+        QUIZ = 50,
+        KBA = 100,
+        CALIBRATION = 150,
+        HACKATHON = 200
+    }
 
-     /*
-      * Declare struct AssessmentCard
-      * Structure members should be assessmentType, score and datetime
-      */
+    /*
+     * Declare struct AssessmentCard
+     * Structure members should be assessmentType, score and datetime
+     */
+    public struct AssessmentCard
+    {
+        public AssessmentType assessmentType;
+        public int score;
+        public DateTime dateTime;
+    }
 
     public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
             
             // this code is optional here however it wil help 
             // you to understand the requirements better    
         }
         // this method accepts assessment cards and calculates total percentage
-        public static int GetOverallScore()
+        public static int GetOverallScore(AssessmentCard[] cards)
         {
             // the logic put here should use foreach loop to read 
             // card data and calculate percentage as mentioned.
             // the maximum score value for each assessment type can be retrieve
-            // from enum constant            
+            // from enum constant    
+            int totalScore = 0, maxScore = 0;
+
+            foreach(AssessmentCard card in cards)
+            {
+                maxScore += Enum.GetValues(typeof(card.assessmentType));
+            }
+            return 0;
         }
 
         // this method should generate score card from the details provided
@@ -43,6 +63,7 @@ namespace AssessmentScoreCardGenerator
             // cadet no, cadet name, calibration type, score obtained
 
             // the built string should be returned as string and not as StringBuilder
+            return null;
         }
     }
 
